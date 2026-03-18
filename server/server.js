ec1ab42fcbf648de942b0b448b1d8c73ba366e1b
ryongbom <ryongbom@mail.ru>
2026-03-18 21:09:02 +0800

    level2, level3 completed

--- server/server.js
+++ server/server.js
@@ -14,19 +14,46 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/index.html'));
 });
 
+let userCount = 0;
+
 // Socket connection processing
 io.on('connection', (socket) => {
-    console.log('new user connected:', socket.id);
+    userCount++;
+    console.log(`new user connected:, ${socket.id} (current number of users: ${userCount})`);
+
+    socket.emit('chat message', {
+        type: 'system',
+        content: `✨ 서버에 접속되었습니다. 현재 ${userCount}명이 접속 중입니다.`,
+        timestamp: new Date().toLocaleTimeString()
+    });
+
+    socket.broadcast.emit('chat message', {
+        type: 'system',
+        content: `👋 새로운 사용자가 들어왔습니다. (현재 ${userCount}명)`,
+        timestamp: new Date().toLocaleTimeString()
+    })
 
     socket.on('chat message', (msg) => {
         console.log(`Message from ${socket.id}: ${msg}`);
 
-        socket.emit('chat message', `Echo: ${msg}`);
+        io.emit('chat message', {
+            type: 'user',
+            userId: socket.id,
+            content: msg,
+            timestamp: new Date().toLocaleTimeString()
+        });
     });
 
     // Disconnection processing
     socket.on('disconnect', () => {
-        console.log('user disconnected:', socket.id);
+        userCount--;
+        console.log(`user disconnected: ${socket.id} (current number of users: ${userCount})`);
+
+        io.emit('chat message', {
+            type: 'system',
+            content: `👋 사용자가 나갔습니다. (현재 ${userCount}명)`,
+            timestamp: new Date().toLocaleTimeString()
+        });
     });
 });
 
