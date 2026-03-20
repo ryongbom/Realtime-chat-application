370ccca933f21032965b92e1d59be4d354711679
ryongbom <ryongbom@mail.ru>
2026-03-20 22:07:03 +0800

    level4 start

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const path = require('path');
+const { timeStamp } = require('console');
 
 const app = express();
 const server = http.createServer(app);
@@ -14,51 +15,33 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/index.html'));
 });
 
-let userCount = 0;
-
-// Socket connection processing
 io.on('connection', (socket) => {
-    userCount++;
-    console.log(`new user connected:, ${socket.id} (current number of users: ${userCount})`);
+    console.log('connected to Server', socket.id);
 
-    socket.emit('chat message', {
-        type: 'system',
-        content: `✨ 서버에 접속되었습니다. 현재 ${userCount}명이 접속 중입니다.`,
-        timestamp: new Date().toLocaleTimeString()
-    });
-
-    socket.broadcast.emit('chat message', {
-        type: 'system',
-        content: `👋 새로운 사용자가 들어왔습니다. (현재 ${userCount}명)`,
-        timestamp: new Date().toLocaleTimeString()
-    })
-
-    socket.on('chat message', (msg) => {
-        console.log(`Message from ${socket.id}: ${msg}`);
+    socket.on('chat message', (data) => {
+        console.log('received message: ', data.content);
 
         io.emit('chat message', {
             type: 'user',
-            userId: socket.id,
-            content: msg,
-            timestamp: new Date().toLocaleTimeString()
+            content: data.content,
+            userId: data.userId,
+            timeStamp: new Date().toLocaleDateString()
         });
     });
 
-    // Disconnection processing
-    socket.on('disconnect', () => {
-        userCount--;
-        console.log(`user disconnected: ${socket.id} (current number of users: ${userCount})`);
-
-        io.emit('chat message', {
-            type: 'system',
-            content: `👋 사용자가 나갔습니다. (현재 ${userCount}명)`,
-            timestamp: new Date().toLocaleTimeString()
+    socket.on('nickname', (data) => {
+        io.emit('system nick', {
+            type: data.type,
+            nickname: data.nickname,
+            timeStamp: data.timeStamp
         });
+    })
+
+    socket.on('disconnect', () => {
+        console.log('disconnected from Server', socket.id);
     });
 });
 
-// Server is starting
-const PORT = 3000;
-server.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
+server.listen(3000, () => {
+    console.log('Server is running at http://localhost:3000');
 });
