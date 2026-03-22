bb90aab10a076ec46b169cc8f0c1ed31a277f702
ryongbom <ryongbom@mail.ru>
2026-03-22 21:38:16 +0800

    main socket.io completed

--- server/server.js
+++ server/server.js
@@ -15,30 +15,57 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/index.html'));
 });
 
+const users = new Map();
+
 io.on('connection', (socket) => {
     console.log('connected to Server', socket.id);
 
+    socket.on('nickname', (data) => {
+        users.set(socket.id, { nickname: data.nickname });
+
+        console.log(`👤 ${data.nickname} joined (${socket.id})`);
+
+        io.emit('system message', {
+            type: 'system',
+            content: `${data.nickname} joined the chat`,
+            timeStamp: data.timeStamp
+        });
+
+        socket.emit('system message', {
+            type: 'system message',
+            content: `Welcome ${data.nickname}`,
+            timeStamp: new Date().toLocaleTimeString()
+        });
+    });
+
     socket.on('chat message', (data) => {
-        console.log('received message: ', data.content);
+        const user = users.get(socket.id);
+        const nickname = user ? user.nickname : 'Unknown';
 
         io.emit('chat message', {
             type: 'user',
             content: data.content,
             userId: data.userId,
-            timeStamp: new Date().toLocaleDateString()
+            nickname: nickname,
+            timeStamp: new Date().toLocaleTimeString()
         });
     });
 
-    socket.on('nickname', (data) => {
-        io.emit('system nick', {
-            type: data.type,
-            nickname: data.nickname,
-            timeStamp: data.timeStamp
-        });
-    })
-
     socket.on('disconnect', () => {
-        console.log('disconnected from Server', socket.id);
+        const user = users.get(socket.id);
+        if (user) {
+            console.log(`${user.nickname} disconnected (${socket.id})`);
+
+            io.emit('system message', {
+                type: 'system',
+                content: `${user.nickname} left the chat`,
+                timeStamp: new Date().toLocaleTimeString()
+            });
+
+            users.delete(socket.id);
+        } else {
+            console.log(`Unknown user disconnected (${socket.id})`);
+        }
     });
 });
 
